e1f6066e9b8e01f2c31beb53ce25929dce27d817
Lean Rada <godffrey0@gmail.com>
2015-07-03 00:43:50 +0800

    Lazy loading of family relation collections

--- js/model/Family.js
+++ js/model/Family.js
@@ -5,20 +5,20 @@ define(["backbone", "model/FamilyMemberCollection"], function(Backbone, FamilyMe
 			name: null,
 		},
 
-		initialize: function() {
-			var that = this;
-
-			this.members = null;
-			this.on("change:id", function(model, value, options) {
-				that.members = new FamilyMemberCollection(null, {family_id: value});
-			}, this);
-			
-			if(!this.isNew()) {
-				that.members = new FamilyMemberCollection(null, {family_id: this.get("id")});
-			}
-		},
-
 		get_members: function() {
+			if(this.members === undefined) {
+				var that = this;
+
+				this.members = null;
+				this.on("change:id", function(model, value, options) {
+					that.members = new FamilyMemberCollection(null, {family_id: value});
+				}, this);
+				
+				if(!this.isNew()) {
+					that.members = new FamilyMemberCollection(null, {family_id: this.get("id")});
+				}
+			}
+
 			return this.members;
 		},
 	});
--- js/model/Official.js
+++ js/model/Official.js
@@ -7,20 +7,20 @@ define(["backbone", "model/OfficialFamilyCollection"], function(Backbone, Offici
 			nickname: null,
 		},
 
-		initialize: function() {
-			var that = this;
-
-			this.families = null;
-			this.on("change:id", function(model, value, options) {
-				that.families = new OfficialFamilyCollection(null, {official_id: value});
-			}, this);
-			
-			if(!this.isNew()) {
-				this.families = new OfficialFamilyCollection(null, {official_id: this.get("id")});
-			}
-		},
-
 		get_families: function() {
+			if(this.families === undefined) {
+				var that = this;
+				
+				this.families = null;
+				this.on("change:id", function(model, value, options) {
+					that.families = new OfficialFamilyCollection(null, {official_id: value});
+				}, this);
+				
+				if(!this.isNew()) {
+					this.families = new OfficialFamilyCollection(null, {official_id: this.get("id")});
+				}
+			}
+
 			return this.families;
 		},
 
