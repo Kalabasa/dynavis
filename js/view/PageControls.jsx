32bc3a0116389a3c853bb5a8a507aedafd66330c
Lean Rada <godffrey0@gmail.com>
2015-07-04 02:33:44 +0800

    Make PageControls control the PageableCollection

--- js/view/PageControls.jsx
+++ js/view/PageControls.jsx
@@ -4,11 +4,27 @@ define(["react", "react.backbone"], function(React) {
 		render: function() {
 			return (
 				<div>
-					<button onClick={this.props.onPrev}>Prev</button>
+					<button onClick={this.handle_prev}>Prev</button>
 					Page {this.collection().getPage() + 1} of {this.collection().getTotalPages()}
-					<button onClick={this.props.onNext}>Next</button>
+					<button onClick={this.handle_next}>Next</button>
 				</div>
 			);
+		},
+
+		handle_prev: function() {
+			if(this.props.onPrev) {
+				this.props.onPrev();
+			}else{
+				this.collection().prev();
+			}
+		},
+
+		handle_next: function() {
+			if(this.props.onNext) {
+				this.props.onNext();
+			}else{
+				this.collection().next();
+			}
 		},
 	});
 });
