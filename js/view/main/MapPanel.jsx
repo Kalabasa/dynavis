db40e32d03372ea271d77d483db38a5934a47adc
Lean Rada <godffrey0@gmail.com>
2015-07-07 00:21:02 +0800

    Zoom LOD

--- js/view/main/MapPanel.jsx
+++ js/view/main/MapPanel.jsx
@@ -2,6 +2,8 @@
 define(["react", "leaflet"], function(React, L) {
 	return React.createClass({
 		componentDidMount: function() {
+			var that = this;
+
 			var tile_layer = L.tileLayer("http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={access_token}", {
 				attribution: "Map data &copy; <a href='http://openstreetmap.org'>OpenStreetMap</a> contributors, <a href='http://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='http://mapbox.com'>Mapbox</a>",
 				mapid: "mapbox.streets-basic",
@@ -20,23 +22,59 @@ define(["react", "leaflet"], function(React, L) {
 				],
 			});
 
-			$.get("json/Regions.json").success(function(data) {
-				L.geoJson(data, {
-					smoothFactor: 2.0,
-					weight: 3,
-					color: "#77cc00",
-				}).addTo(map);
-			});
+			this.saved_layers = {};
+			this.current_layer_name = null;
+			this.current_layer = null;
+			this.set_layer("json/Regions.json");
+
+			this.map.on("zoomend", this.on_zoom);
 		},
 
 		componentWillUnmount: function() {
 			this.map = null;
+			this.current_layer = null;
 		},
 
 		render: function() {
 			return (
 				<div className="map-panel"></div>
 			);
+		},
+
+		set_layer: function(layer) {
+			var that = this;
+
+			if(this.current_layer_name === layer) return;
+			this.current_layer_name = layer;
+
+			if(this.current_layer) {
+				this.map.removeLayer(this.current_layer);
+				this.current_layer = null;
+			}
+
+			var props = {
+				smoothFactor: 2.0,
+				weight: 3,
+				color: "#77cc00",
+			};
+
+			if(this.saved_layers[layer]) {
+				this.current_layer = this.saved_layers[layer];
+				this.map.addLayer(this.current_layer);
+			}else{
+				$.get(layer).success(function(data) {
+					that.saved_layers[layer] = that.current_layer = L.geoJson(data, props)
+					that.map.addLayer(that.current_layer);
+				});
+			}
+		},
+
+		on_zoom: function() {
+			if(this.map.getZoom() >= 9) {
+				this.set_layer("json/Provinces.json");
+			}else{
+				this.set_layer("json/Regions.json");
+			}
 		},
 	});
 });
