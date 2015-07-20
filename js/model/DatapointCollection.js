9db5d751a3a288f6e00223a289b5d06a311a17be
Lean Rada <godffrey0@gmail.com>
2015-07-20 11:16:28 +0800

    ALlow finding all datapoints by area,year

--- js/model/DatapointCollection.js
+++ js/model/DatapointCollection.js
@@ -12,26 +12,30 @@ define(["underscore", "backbone", "model/Datapoint"], function(_, Backbone, Data
 			return data.data;
 		},
 
-		find_datapoint: function(area_code, year) {
-			year = parseInt(year).toString();
+		find_datapoints: function(area_code, year, single) {
 			area_code = ("000000000" + area_code).slice(-9);
+			year = parseInt(year).toString();
+
 			// var reg_code = area_code.substr(0,2);
 			var prov_code = area_code.substr(2,2);
 			var mun_code = area_code.substr(4,2);
 			var bar_code = area_code.substr(6,3);
+			
 			var match = prov_code == "00" ? [0,2] :
 				mun_code == "00" ? [2,2] :
 				bar_code == "000" ? [2,4] : [2,7];
 			var area_code_match = area_code.substr(match[0], match[1]);
-			return this.find(function(p) {
+			
+			return (single ? this.find : this.filter)(function(p) {
 				return p.get("year") == year
 					&& ("0"+p.get("area_code")).substr(match[0]-9,match[1]) == area_code_match;
 			});
 		},
 
 		get_value: function(area_code, year) {
-			var datapoint = this.find_datapoint(area_code, year);
+			var datapoint = this.find_datapoints(area_code, year, true);
 			if(!datapoint) return null;
+			
 			var value = parseFloat(datapoint.get("value"));
 			return isNaN(value) ? null : value;
 		},
