f56c3482f42b97067b0ef0d4475c575fccb1da2b
Lean Rada <godffrey0@gmail.com>
2015-07-27 06:03:48 +0800

    Revert "Change wording"
    This reverts commit 3f0410d48e488bf68401c4bff845ab302445de57.

--- js/view/main/DatasetBox.jsx
+++ js/view/main/DatasetBox.jsx
@@ -51,9 +51,9 @@ define(["underscore", "react", "jsx!view/SliderTransitionGroupChild", "jsx!view/
 								<div className="field text pad">{this.model().get("description")}</div>
 							</div>
 							<div className="pure-u-1-6">
-								<button className="pull-right button button-flat" onClick={this.handle_edit}>Edit description</button>
+								<button className="pull-right button button-flat" onClick={this.handle_edit}>Edit</button>
 								<a href={"#datasets/" + this.model().get("username") + "/" + this.model().id}>
-									<button className="pull-right button button-flat">Update data</button>
+									<button className="pull-right button button-flat">Update Data</button>
 								</a>
 							</div>
 						</div>
