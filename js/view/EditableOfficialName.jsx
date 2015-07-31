bbef3384e1fd2fdf05df9604f8f6f51fe23ded69
Lean Rada <godffrey0@gmail.com>
2015-07-31 19:01:18 +0800

    Expose inner inputs of editable official name

--- js/view/EditableOfficialName.jsx
+++ js/view/EditableOfficialName.jsx
@@ -17,18 +17,22 @@ define(["react", "react.backbone"], function(React) {
 				<span className={this.props.className}>
 					<label className="pure-u-1 pad">
 						<div className="label">Surname</div>
-						<input className="pure-u-1" type="text" valueLink={this.linkState("surname")} required autoFocus={this.props.autoFocus} />
+						<input ref="surname" className="pure-u-1" type="text" valueLink={this.linkState("surname")} required autoFocus={this.props.autoFocus} />
 					</label>
 					<label className="pure-u-1 pad">
 						<div className="label">Name</div>
-						<input className="pure-u-1" type="text" valueLink={this.linkState("name")} required />
+						<input ref="name" className="pure-u-1" type="text" valueLink={this.linkState("name")} required />
 					</label>
 					<label className="pure-u-1 pad">
 						<div className="label">Nickname</div>
-						<input className="pure-u-1" type="text" valueLink={this.linkState("nickname")} />
+						<input ref="nickname" className="pure-u-1" type="text" valueLink={this.linkState("nickname")} />
 					</label>
 				</span>
 			);
 		},
+
+		get_input_surname: function() { return this.refs.surname; },
+		get_input_name: function() { return this.refs.name; },
+		get_input_nickname: function() { return this.refs.nickname; },
 	});
 });
