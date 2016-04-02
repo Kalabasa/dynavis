abc46e5a42dd1198629357836220233016d59f87
Kalabasa <godffrey0@gmail.com>
2016-04-02 15:24:31 +0800

    Fixed scroll edge fade.

--- js-src/view/main/DatasetChooser.jsx
+++ js-src/view/main/DatasetChooser.jsx
@@ -11,6 +11,14 @@ define(function(require) {
 			};
 		},
 
+		componentDidMount: function() {
+			React.findDOMNode(this.refs.dataset_list).addEventListener("scroll", this.handle_scroll);
+		},
+
+		componentWillUnmount: function() {
+			React.findDOMNode(this.refs.dataset_list).removeEventListener("scroll", this.handle_scroll);
+		},
+
 		render: function() {
 			var that = this;
 			return (
@@ -20,12 +28,13 @@ define(function(require) {
 					</div>
 					<SearchControls collection={this.collection()}/>
 					<div className="dataset-chooser-list-container">
-						<div className="dataset-chooser-list">
+						<div ref="dataset_list" className="dataset-chooser-list">
 							{this.collection().map(function(dataset) {
 								return <DatasetChoice key={dataset.cid} model={dataset} selected={that.state.selected===dataset} onClick={function(){ that.click_dataset(dataset); }}/>;
 							})}
 						</div>
-						<div className="scroll-edge-fade"></div>
+						<div ref="scroll_edge_fade_top" className="scroll-edge-fade edge-fade-top"></div>
+						<div ref="scroll_edge_fade_bottom" className="scroll-edge-fade edge-fade-bottom"></div>
 					</div>
 					<div className="clearfix">
 						<button className="pull-right button button-primary" onClick={this.handle_select} disabled={!this.state.selected}>Select</button>
@@ -49,6 +58,28 @@ define(function(require) {
 
 		handle_select: function() {
 			this.props.onSelect(this.state.selected);
+		},
+
+		handle_scroll: function() {
+			var $top_edge = $(React.findDOMNode(this.refs.scroll_edge_fade_top));
+			var $bottom_edge = $(React.findDOMNode(this.refs.scroll_edge_fade_bottom));
+			var $dataset_list = $(React.findDOMNode(this.refs.dataset_list));
+
+			var threshold = 110;
+			var top_offset = $dataset_list.scrollTop();
+			var bottom_offset = $dataset_list[0].scrollHeight - (top_offset + $dataset_list.innerHeight());
+
+			if (top_offset < threshold) {
+				$top_edge.fadeTo(0, top_offset / threshold);
+			} else {
+				$top_edge.fadeTo(0, 1);
+			}
+
+			if (bottom_offset < threshold) {
+				$bottom_edge.fadeTo(0, bottom_offset / threshold);
+			} else {
+				$bottom_edge.fadeTo(0, 1);
+			}
 		},
 	});
 });
